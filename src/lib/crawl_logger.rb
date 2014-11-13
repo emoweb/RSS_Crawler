15296ff815f7a0ac9c838f2af9085815e7bf8902
emo <emo-webs@outlook.com>
2014-11-14 03:02:28 +0900

    2ch crawlerから移植

---
+++ src/lib/crawl_logger.rb
@@ -0,0 +1,87 @@
+#!ruby -Ku
+
+require 'mutex_m'
+
+# Crawlログ(csv)の作成用.
+# Loggerの場合は色々と不便なため作成.
+# マルチスレッドでの同期等を実装.
+class CrawlLogger
+  include Mutex_m
+  
+  def self.close_file f; proc{ f.close unless f.closed? }; end
+  
+  # loglevelは0:error, 1:warn, 2:info, 3:debug. ex)2ならdebugはカット
+  def initialize logfile, stdout = true, loglevel = 2
+    super() # Mutex_mの初期化
+    
+    @loglevel = loglevel
+    f = open(logfile, "a")
+    @out = [f]
+    @out << STDOUT if stdout
+    @def_progname = " "
+    @linelog = [] # 行数単位で読めるように
+    w2 "date,progname,severity,message"
+    
+    ObjectSpace.define_finalizer(self, self.class.close_file(f))
+  end
+  attr_accessor :def_progname, :loglevel
+  attr_reader :linelog
+  
+  # write out
+  def w msg
+    mu_synchronize{ @out.each{ |o| o.puts msg } }
+  end
+  
+  # debug write out. syncでの待ち時間を表示する.
+  # nextlineは2行以上のログのため.
+  def w2 mes, nextline = ""
+    t = Time.now
+    mu_synchronize{
+      mes = "%s,%.3fs late%s" % [mes, (Time.now - t), nextline]
+      @out.each{ |o| o.puts mes }
+      @linelog << mes
+    }
+  end
+  
+  
+  def log severity, progname = @def_progname, &log_mes_gen
+    datetime = Time.now
+    
+    mes, nextline = if (logdata = log_mes_gen.call).is_a?(Enumerable) then
+      a = logdata.to_a
+      n = a[1..-1].collect {|s| "-,-,-,#{s},-" }.join("\n")
+      [a[0], "\n" + n]
+    else
+      [logdata, ""]
+    end
+    
+    w2("%s.%03d,%s,%s,%s" % [
+      (datetime.strftime '%H:%M:%S'), (datetime.usec / 1000),
+      progname, severity, mes
+    ], nextline)
+  end
+  
+  # 記録用
+  def error progname = @def_progname, &log_mes_gen
+    log("ERROR", progname, &log_mes_gen)
+  end
+  def warn progname = @def_progname, &log_mes_gen
+    log("WARN", progname, &log_mes_gen) if 0 < @loglevel
+  end
+  def info progname = @def_progname, &log_mes_gen
+    log("INFO", progname, &log_mes_gen) if 1 < @loglevel
+  end
+  def debug progname = @def_progname, &log_mes_gen
+    log("DEBUG", progname, &log_mes_gen) if 2 < @loglevel
+  end
+  
+  def error_exception place, exception, progname = @def_progname
+    error(progname){ "#{place} : #{exception.inspect} in #{$@.inspect}" }
+  end
+  
+end
+
+
+
+
+
---
+++ src/lib/httputil.rb
@@ -0,0 +1,146 @@
+#!ruby -Ku
+# HTTP関連の処理のまとめ
+
+require 'net/http'
+require 'uri'
+require 'kconv'
+require 'time'
+Net::HTTP.version_1_2
+
+module HTTPUtil
+  # ブラウザごとのUserAgent
+  USER_AGENT = {
+    :ie10 => 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)',
+  }
+  
+  # 標準で送るヘッダ
+  DEFAULT_HAEDER_GET = {
+    "User-Agent" => USER_AGENT[:ie10],
+    # Accept-Encoding, Accept, Hostは勝手につけてくれる
+  }
+  
+  
+  #       header_if_modified_since(date, base_header) -> Hash
+  # 
+  # if-modified-sinceを使うget時のHTTPヘッダを生成する.
+  # dateにDateを渡した場合はその時間,
+  # Stringを渡した場合はそのファイルの更新日時を使う.
+  def header_if_modified_since date, base_header = DEFAULT_HAEDER_GET.dup
+    date = case date
+    when String; File.mtime(date)
+    when Date; date
+    end
+    
+    base_header["If-Modified-Since"] = date.httpdate
+    base_header
+  end
+  module_function(:header_if_modified_since)
+  
+  
+  #       get_for_cache(url, cache_file, referer, header) -> Net::HTTPResponse
+  # 
+  # cacheからの変更があるか調べてDLする.
+  # 変更なしの場合はcacheから読み込み,変更がある場合はcacheに書き込む
+  def get_and_cache url, cache_file, referer = nil, header = DEFAULT_HAEDER_GET.dup
+    cache_exists = File.exists?(cache_file)
+    header = header_if_modified_since(cache_file, header) if cache_exists
+    res = get(url, referer, header)
+    res.body = IO.binread(cache_file) if(cache_exists && res.status == 304)
+    IO.binwrite(cache_file, res.body) if((res.status / 100) == 2)
+    return res
+  end
+  module_function(:get_and_cache)
+  
+  
+  #       request(url, method, *arg) -> Net::HTTPResponse
+  # 
+  # HTTPのrequest_XXをURIパースして呼び出すためのwrapper
+  def request url, method, *arg
+    u = URI url
+    pt = u.path
+    pt = "/" if pt.empty?
+    pt += ('?' + u.query) if u.query
+    pt += ('#' + u.fragment) if u.fragment
+    Net::HTTP.new(u.host, u.port).__send__(method, pt, *arg)
+  end
+  module_function :request
+  
+  
+  #       get(url, referer, header) -> Net::HTTPResponse
+  # 
+  # HTTPでGetする. 圧縮/解凍は勝手に処理してくれる.
+  # refererがnilなら無し.
+  # headerはnilなら標準.
+  def get url, referer = nil, header = DEFAULT_HAEDER_GET.dup
+    header = DEFAULT_HAEDER_GET.dup unless header
+    #p [url,referer,header]
+    header["Referer"] = referer.to_s if referer
+    request(url, :get, header)
+  end
+  module_function :get
+  
+  
+  #       post(url, data) -> Net::HTTPResponse
+  # 
+  # HTTPでpostする. 内容はdataに指定.
+  def post url, data, header = PostHeader
+    request url, :request_post, data, header
+  end
+  module_function :post
+  
+end
+
+
+# 独自の拡張メソッドをHTTP Responseに定義
+
+class Net::HTTPResponse
+  # Content-Typeに対応する拡張子. 存在しない場合はContent-Typeに等しい.
+  FILE_EXTENSION = {
+    "plain" => "txt", "richtext" => "rtf",
+    "msword" => "doc", "javascript" => "js", "shockwave-flash" => "swf",
+    "jpeg" => "jpg", "MP4A-LATM" => "m4a", "MP4V-ES" => "mp4",
+    "quicktime" => "qt", "msvideo" => "avi",
+    "ms-bmp" => "bmp",
+  }
+  # subtypeに対応するextensionを返す
+  def self.get_extension subtype
+    st = subtype.sub(/^x\-/,"")
+    ext = FILE_EXTENSION[st] || st
+    ".#{ext}"
+  end
+  
+  
+  #       split_content_type -> [Symbol,String,String] | nil
+  # 
+  # content_typeを解析して type, subtype, extension を返す.
+  # 既に解析されている場合はそのまま.
+  # e.g. 'text/html' => [ :text, "html", ".html" ]
+  # content_typeがnilならnil
+  def split_content_type
+    @content_type ||= begin
+      ct = content_type
+      ct && (%r!^(\w+)/([\w\.\-]+)! =~ ct) &&
+        [ $1.to_sym, $2, self.class.get_extension($2)]
+    end
+  end
+  
+  
+  # type(e.g. :text)だけを返す
+  def c_type
+    split_content_type && split_content_type[0]
+  end
+  
+  # ファイルの拡張子だけを返す. => nil | String
+  def file_extension
+    split_content_type && split_content_type[2]
+  end
+  
+  # utf8に変換したbodyを返す
+  def text; body && body.toutf8; end
+  
+  # ステータスコードを数値型で返す.
+  def status; code.to_i; end
+  
+end
+
+
