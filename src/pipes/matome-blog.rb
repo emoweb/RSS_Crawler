e1a9b9c80096ec64efec7c554db6f31dd331dcb5
emo <emo-webs@outlook.com>
2014-11-19 00:46:40 +0900

    痛いニュース追加

---
+++ src/pipes/matome-blog.rb
@@ -0,0 +1,20 @@
+#!ruby -Ku
+
+# 2ch等のまとめblog
+
+# 痛いニュース(ﾉ∀`)
+# http://blog.livedoor.jp/dqnplus/
+RSS_PIPES[:dqnplus] = Proc.new{ |pipe|
+  pipe.procedure_1(
+    'http://blog.livedoor.jp/dqnplus/index.rdf',
+    nil,
+    '//div[@class="blogbody"]'
+  ) {|h|
+    #IO.write("Z:/#{Time.now.to_i}.html", h)
+    h.gsub!(/[\r\n]+/, '')
+    h =~ %r#<\!-- google_ad_section_start -->(.*?)<div class="posted"></div>#
+    $1
+  }
+}
+
+
