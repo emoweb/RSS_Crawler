756aa56cf3414897e3d4395f80dc33c1158b0f8b
emo <emo-webs@outlook.com>
2015-03-01 01:54:51 +0900

    readme更新

--- src/pipes/simple_pipes.rb
+++ src/pipes/simple_pipes.rb
@@ -11,14 +11,3 @@ conf[:pipe_procedure].each{|name, opt|
     pipe.pipe_procedure(opt)
   }
 }
-
-# pipe_procedure_1 だけで完結する場合
-# [[name,url,filter_regex,xpath]] を読み込み定義
-#conf[:procedure_1].each{ |name, url, fr, xp|
-#  args = [url, (fr.empty? ? nil : Regexp.new(fr)), (xp.empty? ? nil : xp)]
-#  RSS_PIPES[name.to_sym] = Proc.new{|pipe| pipe.procedure_1(*args) }
-#}
-
-# pipe_procedure_1による取得と,正規表現による本文抜き出しを行う場合
-
-
