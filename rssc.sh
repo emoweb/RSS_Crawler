1cfe94000f756d6707ee494e162b4cd5faad1827
emo <emo-webs@outlook.com>
2017-03-31 07:54:14 +0900

    GitHub Pagesへの公開を自動化

--- rssc.sh
+++ rssc.sh
@@ -4,6 +4,10 @@
 r=`ping -c 1 -w 1 www.google.com`
 if [ ! -z "$r" ]; then
   ${0%/*}/src/rss_crawler.rb
+  
+  # 出力をGitHub Pagesへcommit
+  pushd /data/var/rssc_log/
+  git add . && git commit -m "update" && git push
 fi
 
 
